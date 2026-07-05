92e5bd685714afa3f4b355b26539cbc6accb0879
DominikHebda <hebdad@wp.pl>
2026-07-05 15:36:13 +0200

    "Setup Django REST Framework with Swagger documentation using drf-spectacular"

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # schema JSON
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
