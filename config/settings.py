86f97fa6c1161c80479e818beb17866b1cc7fbbb
DominikHebda <hebdad@wp.pl>
2026-07-12 19:46:30 +0200

    feat(api): add filtering, search, ordering and pagination support

--- config/settings.py
+++ config/settings.py
@@ -144,9 +144,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    
-    "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "common.pagination.DefaultPagination",
 }
 
 SPECTACULAR_SETTINGS = {
