ff8ec3adb873c63edb9e2129c5e87e1997c4ecf6
DominikHebda <hebdad@wp.pl>
2026-07-28 18:41:14 +0200

    test: fix timezone-aware visit factory dates

--- tests/factories/visits.py
+++ tests/factories/visits.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 import factory
+from django.utils import timezone
 
 from tests.factories.accounts import UserFactory
 from tests.factories.animals import AnimalFactory
@@ -12,7 +15,9 @@ class VisitFactory(factory.django.DjangoModelFactory):
     animal = factory.SubFactory(AnimalFactory)
     veterinarian = factory.SubFactory(UserFactory)
 
-    visit_date = factory.Faker("future_datetime")
+    visit_date = factory.LazyFunction(
+        lambda: timezone.now() + timedelta(days=7)
+    )
     reason = factory.Faker("sentence", nb_words=4)
     notes = factory.Faker("paragraph")
     status = Visit.Status.SCHEDULED
