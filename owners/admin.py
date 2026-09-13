06529685513ac2d9505008c6112916e9d529ec1d
DominikHebda <hebdad@wp.pl>
2026-09-13 20:12:32 +0200

    Register owners in Django admin

--- owners/admin.py
+++ owners/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Owner
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "last_name",
+        "first_name",
+        "email",
+        "phone",
+        "created_at",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+    )
+    ordering = (
+        "last_name",
+        "first_name",
+    )
