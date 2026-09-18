fe5446956b5f14264273ba9231b3476769a30a4c
DominikHebda <hebdad@wp.pl>
2026-09-18 13:32:05 +0200

    Register visits in Django admin

--- visits/admin.py
+++ visits/admin.py
@@ -1,1 +1,31 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Visit
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = (
+        "animal",
+        "veterinarian",
+        "visit_date",
+        "reason",
+        "status",
+    )
+    list_filter = (
+        "status",
+        "visit_date",
+        "veterinarian",
+    )
+    search_fields = (
+        "animal__name",
+        "veterinarian__username",
+        "veterinarian__first_name",
+        "veterinarian__last_name",
+        "reason",
+    )
+    ordering = ("visit_date",)
+    list_select_related = (
+        "animal",
+        "veterinarian",
+    )
