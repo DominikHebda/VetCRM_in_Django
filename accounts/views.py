2e674ccdb8012c54c6acd03b1807aebda70dc433
DominikHebda <hebdad@wp.pl>
2026-09-19 11:59:55 +0200

    Add veterinarians API endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import CurrentUserSerializer
+from .models import UserProfile
+from .serializers import CurrentUserSerializer, VeterinarianSerializer
 
 
 class CurrentUserView(APIView):
@@ -17,4 +18,23 @@ class CurrentUserView(APIView):
     )
     def get(self, request):
         serializer = CurrentUserSerializer(request.user)
+        return Response(serializer.data)
+
+
+class VeterinarianListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        tags=["Accounts"],
+        summary="List veterinarians",
+        responses=VeterinarianSerializer(many=True),
+    )
+    def get(self, request):
+        user_model = request.user.__class__
+
+        veterinarians = user_model.objects.filter(
+            profile__role=UserProfile.Role.VET,
+        ).order_by("last_name", "first_name", "username")
+
+        serializer = VeterinarianSerializer(veterinarians, many=True)
         return Response(serializer.data)
