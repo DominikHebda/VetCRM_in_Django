b1ad7c9215b4a08b68846866c0e02b5d8ccdc125
DominikHebda <hebdad@wp.pl>
2026-09-20 12:31:36 +0200

    Add animal details to medical record API

--- medical/serializers.py
+++ medical/serializers.py
@@ -4,6 +4,19 @@ from .models import MedicalRecord
 
 
 class MedicalRecordSerializer(serializers.ModelSerializer):
+    animal_name = serializers.CharField(
+        source="visit.animal.name",
+        read_only=True,
+    )
+    animal_species = serializers.CharField(
+        source="visit.animal.species",
+        read_only=True,
+    )
+    animal_owner_name = serializers.CharField(
+        source="visit.animal.owner.__str__",
+        read_only=True,
+    )
+
     class Meta:
         model = MedicalRecord
         fields = "__all__"
