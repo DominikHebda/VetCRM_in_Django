9ea3a378c862789a30a84a3bb6cc744c29403dc9
DominikHebda <hebdad@wp.pl>
2026-09-20 22:23:26 +0200

    Register medical records in Django admin

--- medical/admin.py
+++ medical/admin.py
@@ -1,1 +1,27 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import MedicalRecord
+
+
+@admin.register(MedicalRecord)
+class MedicalRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "visit",
+        "diagnosis",
+        "weight",
+        "temperature",
+        "created_at",
+    )
+    search_fields = (
+        "visit__animal__name",
+        "visit__animal__owner__first_name",
+        "visit__animal__owner__last_name",
+        "diagnosis",
+        "treatment",
+    )
+    ordering = ("-created_at",)
+    list_select_related = (
+        "visit",
+        "visit__animal",
+        "visit__animal__owner",
+    )
